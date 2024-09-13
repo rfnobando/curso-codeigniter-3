e674033979fbd04e13fa0ed40c34a92ee3a9ad2d
Rodrigo <rfnobando@gmail.com>
2024-09-13 06:17:52 -0300

    first commit

---
+++ products-insert.sql
@@ -0,0 +1,6 @@
+insert into products (name, price) values
+('Cereales', 350.53),
+('Galletitas de agua', 220.40),
+('Jugo de naranja', 150.00),
+('Yogur bebible', 180.25),
+('Puré de tomate', 120.00);
